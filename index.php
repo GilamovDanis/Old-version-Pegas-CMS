a12047019963b44064bb30a6638b0bbf14b33666
GilamovDanis <ds-92@yandex.ru>
2012-10-04 19:40:30 +0400

    фыыфввфыфыв

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8" />
+  <title><?php echo $title ?></title>
+  <link type="text/css" href="main.css" rel="stylesheet" />
+ </head> 
+<body>
+<?php 
+echo 'adasd';
+?>
+asdasdasdsadasd
+<body>
+</html>
