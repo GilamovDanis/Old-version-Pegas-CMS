ca783e73b4b1033b57eab32588494db4e048d9d8
GilamovDanis <ds-92@yandex.ru>
2012-10-06 23:24:38 +0400

    Guestbook new version

--- themes/default/views/guestbook/add.php
+++ themes/default/views/guestbook/add.php
@@ -1,7 +1,18 @@
 <h3>Добавление сообщения</h3>
 <?php echo Form::open('/guestbook/add')?>
+<?php if ($user) { ?>
+<p class="s1">
+Вы вошли как <b><?php echo $user->username?></b>, вам не потребуеться вводить свое имя и код с картинки.
+</p>
+<?php } else { ?>
+<p class="s1">Вы вошли как <b>Гость</b>, вам потребуеться дополнительно ввести свое имя и код с картинки. 
+</p>
 <?php echo Form::label('name', 'Введите ваше имя (max 64)') ?><br />
 <?php echo Form::input('name', HTML::chars(Arr::get($_POST, 'name')))?> <br />
+<?php echo Form::label('captcha', 'Введите символы с картинки')?> <br />
+<?php echo $view_captcha; ?> <br />
+<?php echo Form::input('captcha', FALSE,array('size'=>4))?> <br />
+<?php } ?>
 <?php echo Form::label('content', 'Введите ваше сообщение (max 1024)') ?>  <br />
 <?php echo Form::textarea('content', HTML::chars(Arr::get($_POST, 'content')))?> <br />
 <?php echo Form::submit('save','Добавить')?>
--- themes/default/views/guestbook/main.php
+++ themes/default/views/guestbook/main.php
@@ -1,22 +1,33 @@
-<h2>Гостевая книга</h2> <br />
-<div class="post">
+<div id="content">
+<div class="content-title">Гостевая книга</div>
+<div class="links">
+<?php echo HTML::image('themes/default/icons/refresh.png', array('alt' => 'img','style'=>'vertical-align:middle')); ?> <?php echo HTML::anchor('/guestbook/', 'Обновить'); ?>
+</div>
+
+<table class="post">
 	<?php foreach ($messages as $message) { ?>
 	<?php if($message->id%2==0) { ?>
-	<div class="post_1">
+	<tr class="post1">
 	<?php } else { ?>
-	<div class="post_2">
+	<tr class="post2">
 	<?php } ?>
+		<td width="54">
+		<?php echo HTML::image('themes/default/icons/ava.png', array('alt' => 'Аватар')) ?>
+		</td>
+		<td width="520">
 		<div class="post_title">
-		<?php echo $message->name; ?>
-			<?php if(Auth::instance()->logged_in('admin')) { ?>
-			<?php echo HTML::anchor('/guestbook/delete/'.$message->id, ' [delete]'); ?>
-			<?php } ?>
+		<b><?php echo $message->name; ?></b> - <?php echo Date::timef($message->created)?>
 		</div>
 		<div class="post_content">
 		<?php echo $message->content ?>
 		</div>
-	</div>
+			<?php if(Auth::instance()->logged_in('admin')) { ?>
+			<?php echo HTML::anchor('/guestbook/delete/'.$message->id, ' [delete]'); ?>
+			<?php } ?>
+		</td>
+	</tr>
 	<?php } ?>
-</div>
-<?php echo $pagination ?>
+</table>
+<?php echo $pagination ?> <br />
 <?php echo View::factory('/guestbook/add'); ?>
+</div>
