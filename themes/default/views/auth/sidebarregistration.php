23ca2cb3476dd17ca1cbc068607ac343184d79bf
GilamovDanis <ds-92@yandex.ru>
2012-10-23 20:35:29 +0400

    New

---
+++ themes/default/views/auth/sidebarregistration.php
@@ -0,0 +1,2 @@
+<h2>Правила регистрации</h2>
+<?php echo View::factory('/main/rulesreg'); ?>
---
+++ themes/default/views/main/topmenu.php
@@ -0,0 +1,6 @@
+<div id="topmenu">
+		<ul>
+			<li><?php echo HTML::anchor('/#', 'Главная'); ?></li>	
+			<li><?php echo HTML::anchor('/about/', 'О Pegas CMS'); ?></li>	
+		</ul>	
+</div>
